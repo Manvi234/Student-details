97c023f24ca71050f791efb95db649a6c2c6aaab
Manvi234 <manvigawande234@gmail.com>
2023-06-12 11:53:22 +0530

    connected to mongoDB Database

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello NODE API");
 });
 
-app.listen(3000, () => {
-  console.log(`Node Api App is running on port 3000`);
+app.get("/blog", (req, res) => {
+  res.send("Hello Blog");
 });
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    "mongodb+srv://admin:Manvi@studentdetailsapi.cqvx8bi.mongodb.net/StudentDetailsAPI"
+  )
+  .then(() => {
+    console.log("connected to MongoDB");
+    app.listen(3000, () => {
+      console.log(`Node API app is running on port 3000`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
